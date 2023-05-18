754844732dbfa5be190e0a2c66f0b03385931161
Haky86 <hakamelassouad@gmail.com>
2023-05-18 15:01:48 +0200

    a71: Create dummy vendor.prop configs
    Change-Id: Ibc513bcc830e263aa00574731d981d4164f21bf8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,5 +40,8 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_a71
 BOARD_HEADER_VERSION := 2
 TARGET_KERNEL_CONFIG := a71_defconfig
 
+# Properties
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
 # Get non-open-source specific aspects
 include vendor/samsung/a71/BoardConfigVendor.mk
